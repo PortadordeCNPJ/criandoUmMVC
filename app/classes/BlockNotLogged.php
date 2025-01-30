c7191e25698cac6612a0cdbc7a2771ff590dc8f3
PortadordeCNPJ <guilhermedesouzamuller@gmail.com>
2025-01-29 22:00:49 -0300

    Add: Termino das classes de Blocks feitas

--- app/classes/BlockNotLogged.php
+++ app/classes/BlockNotLogged.php
@@ -6,10 +6,12 @@ use app\interfaces\ControllerInterface;
 
 class BlockNotLogged
 {
-    public static function block(ControllerInterface $controllerInterface, array $blockMethods){
+    public static function block(ControllerInterface $controllerInterface, array $blockMethods)
+    {
         $canBlockMethod = Block::getMethodToBlock($controllerInterface, $blockMethods);
 
         if (!isset($_SESSION['user']) and $canBlockMethod) {
+            BlockPostRequest::block();
             return redirect('/');
         }
     }
